8b29631d388c466db81ab5aaf2a01791ff461322
Deepanker Tiwari <deepamamazing@gmail.com>
2026-01-03 10:30:00 +0530

    Nodejs Practice~ Routes practice

--- src/app.js
+++ src/app.js
@@ -3,8 +3,20 @@ const express=require("express")
 const app=express();        //Calling the function
 
 //Request Handler function
+
+
+app.use("/test/2",(req,res)=>{
+    res.send("Hello from the test server22")
+})
+
+
 app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
+    res.send("Hello from the test server")
+})
+
+
+app.use("/",(req,res)=>{
+    res.send("Hello Deepanker")
 })
 
 app.listen(7777,()=>{

7a19a37683f4d99afd730e224b4d037ef1e8dd9c
Deepanker Tiwari <deepamamazing@gmail.com>
2026-01-04 10:30:00 +0530

    Nodejs Practice~ req,res,next

--- src/app.js
+++ src/app.js
@@ -2,32 +2,17 @@ const express=require("express")
 
 const app=express();        //Calling the function
 
-//Request Handler function
-
-
-// app.use("/test/2",(req,res)=>{
-//     res.send("Hello from the test server22")
-// })
-
-
-app.post("/user",(req,res)=>{
-    res.send("This is user call")
+app.use("/user",(req,res,next)=>{
+    console.log("Respone 1")
+    next()      //IT transfer the calling of API res.send will be called after it
+    res.send("1st response API")
+    
+    //Route Handler
+},(req,res,next)=>{
+    console.log("Response 2");
+    next()
+    res.send("2nd response API")
 })
-
-app.use("/test",(req,res)=>{
-    res.send("Hello from the test server")
-})
-
-
-app.get("/a/",(req,res)=>{
-    res.send("AAAA")
-})
-
-
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello Deepanker")
-// })
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777...");

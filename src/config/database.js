cd39d0d206689ea0f6454e20362253703d8ff1e2
Deepanker Tiwari <deepamamazing@gmail.com>
2026-01-11 20:51:24 +0530

    Remove database.js from tracking

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const { mongoose } = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://deepamamazing_db_user:zJzjfVP5PKBJyDiv@testmongoonline.e26xsjc.mongodb.net/devTinders")
-}
-
-
-module.exports=connectDB;
-

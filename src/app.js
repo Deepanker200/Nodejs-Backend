ff22a824c10c667be76bc62fb5d8916dab928901
Deepanker Tiwari <deepamamazing@gmail.com>
2026-01-11 11:09:58 +0530

    Revert "Nodejs Practice~ req,res,next"
    This reverts commit 7a19a37683f4d99afd730e224b4d037ef1e8dd9c.

--- src/app.js
+++ src/app.js
@@ -2,17 +2,32 @@ const express=require("express")
 
 const app=express();        //Calling the function
 
-app.use("/user",(req,res,next)=>{
-    console.log("Respone 1")
-    next()      //IT transfer the calling of API res.send will be called after it
-    res.send("1st response API")
-    
-    //Route Handler
-},(req,res,next)=>{
-    console.log("Response 2");
-    next()
-    res.send("2nd response API")
+//Request Handler function
+
+
+// app.use("/test/2",(req,res)=>{
+//     res.send("Hello from the test server22")
+// })
+
+
+app.post("/user",(req,res)=>{
+    res.send("This is user call")
 })
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the test server")
+})
+
+
+app.get("/a/",(req,res)=>{
+    res.send("AAAA")
+})
+
+
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello Deepanker")
+// })
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777...");

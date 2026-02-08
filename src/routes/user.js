95e9bea80639c6cde7714565ae23fdbe5931a54b
Deepanker Tiwari <deepamamazing@gmail.com>
2026-02-08 13:57:30 +0530

    Nodejs Practice~ Feed API and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router()
 
 const USER_SAFE_DATA = ["firstName", "lastName", "skills", "about", "age", "gender"]
@@ -62,19 +63,42 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit
+
+        const skip = (page - 1) * limit;
+
         const data = await ConnectionRequest.find(
             {
-               $or:[{ fromUserId: loggedInUser._id},{toUserId:loggedInUser._id}],
-                // status: {
-                //     $nin: ["interested", "accepted", "ignored", "rejected"]
-                // }
+                $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
             }
         )
-        .select("fromUserId toUserId")
-        .populate("fromUserId", "firstName")
-        .populate("toUserId", "firstName")
+            .select("fromUserId toUserId")
+        // .populate("fromUserId", "firstName")
+        // .populate("toUserId", "firstName")
 
-        res.json({ data: "Data fetched successully: ", message: data })
+
+        // console.log(data);
+
+        const hideUsersFromFeed = new Set();
+
+        data.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString())
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        // console.log(hideUsersFromFeed);
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+
+        // console.log(users);
+
+        res.json({ data: "Data fetched successully: ", message: users })
 
     } catch (err) {
         res.status(400).json({ message: err.message })

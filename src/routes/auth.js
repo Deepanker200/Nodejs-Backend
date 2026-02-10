8a1c598c19fd58949b638d52060030740654c23c
Deepanker Tiwari <deepamamazing@gmail.com>
2026-02-10 23:10:06 +0530

    Nodejs Practice~ Signup API changes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,8 +29,15 @@ authRouter.post("/signup", async (req, res) => {
             firstName, lastName, emailId, password: passwordHash
         })
 
-        await user.save()
-        res.send("User Added Successfully")
+        const savedUser = await user.save()
+        const token = await user.getJWT();
+
+        res.cookie("token", token,
+            { expires: new Date(Date.now() + 8 * 3600000) }
+        )
+
+
+        res.json({ message: "User Added Successfully", data: savedUser })
     }
     catch (err) {
         res.status(400).send("Error saving the user: " + err.message)
